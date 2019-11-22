a2c893623c34dd2a80086f365b8f78ccd3c544dd
unknown <tcutlip08@gmail.com>
2019-11-21 20:51:35 -0500

    added material kit, and added the nav buttons that dont work yet on nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./App.css";
 import Home from "./pages/Home/Home";
-
 function App() {
   return (
     <>
--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -2,7 +2,34 @@ import React from "react";
 import "./NavBar.css";
 
 const NavBar = props => {
-  return <div className="NavBar">{props.state}</div>;
+  return (
+    <div className="NavBar">
+      <img src={require("../../assets/images/Self Pic.jpg")} alt="Pic of Me" />
+      <hr />
+      <div id="navs">
+        <div>
+          <a href="#" id="home">
+            Home
+          </a>
+        </div>
+        <div>
+          <a href="#" id="bio">
+            Biography
+          </a>
+        </div>
+        <div>
+          <a href="#" id="myWork">
+            All My Work
+          </a>
+        </div>
+        <div>
+          <a href="#" data-toggle="modal" data-target=".bd-example-modal-lg">
+            Resume
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
